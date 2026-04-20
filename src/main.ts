25247d1c43399905064634b8b912ef24b2b7146e
Tanvir286 <tanvirdiu200@gmail.com>
2026-04-20 09:33:16 +0600

    add demo

---
+++ src/main.ts
@@ -0,0 +1,80 @@
+// external imports
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as express from 'express';
+import helmet from 'helmet';
+import { join, resolve } from 'path';
+// internal imports
+import { IoAdapter } from '@nestjs/platform-socket.io';
+import { AppModule } from './app.module';
+import { CustomExceptionFilter } from './common/exception/custom-exception.filter';
+import { TanvirStorage } from './common/lib/Disk/TanvirStorage';
+import appConfig from './config/app.config';
+import { PrismaExceptionFilter } from './common/exception/prisma-exception.filter';
+
+async function bootstrap() {
+ 
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    rawBody: true,
+  });
+
+ 
+  app.useWebSocketAdapter(new IoAdapter(app));
+  app.setGlobalPrefix('api');
+  app.enableCors();
+  app.use(helmet());
+ 
+
+  app.useStaticAssets(join(__dirname, "..", "..", "public"), {
+    index: false,
+    prefix: "/public",
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: false,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
+  
+  app.useGlobalFilters(
+    new CustomExceptionFilter(),
+    new PrismaExceptionFilter(),  
+  );
+
+  // storage setup
+  TanvirStorage.config({
+    driver: 'local',
+    connection: {
+      rootUrl: appConfig().storageUrl.rootUrl,
+      publicUrl: appConfig().storageUrl.rootUrlPublic,
+      awsBucket: appConfig().fileSystems.s3.bucket,
+      awsAccessKeyId: appConfig().fileSystems.s3.key,
+      awsSecretAccessKey: appConfig().fileSystems.s3.secret,
+      awsDefaultRegion: appConfig().fileSystems.s3.region,
+      awsEndpoint: appConfig().fileSystems.s3.endpoint,
+      minio: true,
+    },
+  });
+
+  // swagger
+  const options = new DocumentBuilder()
+    .setTitle(`${process.env.APP_NAME} api`)
+    .setDescription(`${process.env.APP_NAME} api docs`)
+    .setVersion('1.0')
+    .addTag(`${process.env.APP_NAME}`)
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api/docs', app, document);
+ 
+
+  await app.listen(process.env.PORT ?? 4000, '0.0.0.0');
+}
+bootstrap();

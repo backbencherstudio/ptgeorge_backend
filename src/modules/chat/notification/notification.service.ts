96c043eec4a3d2841d05a63d093aa96a8eb12104
Tanvir286 <tanvirdiu200@gmail.com>
2026-06-22 12:16:09 +0600

    7uyu

--- src/modules/chat/notification/notification.service.ts
+++ src/modules/chat/notification/notification.service.ts
@@ -7,38 +7,38 @@ export class NotificationService {
   constructor(private prisma: PrismaService) {}
 
   // Get all notifications for the authenticated user
-  async findAllNotificationsForUser(userId: string) {
-    const notifications = await this.prisma.notification.findMany({
-      where: { receiver_id: userId },
-      orderBy: { created_at: 'desc' },
-    });
+  // async findAllNotificationsForUser(userId: string) {
+  //   const notifications = await this.prisma.notification.findMany({
+  //     where: { receiver_id: userId },
+  //     orderBy: { created_at: 'desc' },
+  //   });
 
-    return {
-      success: true,
-      message: 'Notifications retrieved successfully',
-      data: notifications,
-    }
-  }
+  //   return {
+  //     success: true,
+  //     message: 'Notifications retrieved successfully',
+  //     data: notifications,
+  //   }
+  // }
 
-  // Delete notification by id for the authenticated user
-  async deleteNotificationForUser(
-    notificationId: string, 
-    userId: string
-  ) {
+  // // Delete notification by id for the authenticated user
+  // async deleteNotificationForUser(
+  //   notificationId: string, 
+  //   userId: string
+  // ) {
   
-    const notification = await this.prisma.notification.delete({
-      where: {
-        id: notificationId,
-        receiver_id: userId,
-      },
-    });
+  //   const notification = await this.prisma.notification.delete({
+  //     where: {
+  //       id: notificationId,
+  //       receiver_id: userId,
+  //     },
+  //   });
 
-    return {
-      success: true,
-      message: 'Notification deleted successfully',
-      data: notification,
-    }
-  }
+  //   return {
+  //     success: true,
+  //     message: 'Notification deleted successfully',
+  //     data: notification,
+  //   }
+  // }
 
 
 
